e6c0131845ce4fd7699cda66b3bf5007ac932a66
Daniel_on_WINDOWS <colinss.daniell530@gamil.com>
2023-12-20 15:36:32 +0800

    1.1 - 1.3 Unverified

--- http_server.py
+++ http_server.py
@@ -1,4 +1,7 @@
+# http_server.py
+
 import socket
+import threading
 from request_handler import handle_request
 
 class HttpServer:
@@ -8,13 +11,24 @@ class HttpServer:
 
     def run(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             server_socket.bind((self.host, self.port))
             server_socket.listen(5)
             print(f"Server running on {self.host}:{self.port}")
 
             while True:
                 client_socket, addr = server_socket.accept()
+                client_thread = threading.Thread(target=self.handle_client, args=(client_socket, addr))
+                client_thread.start()
+
+    def handle_client(self, client_socket, addr):
+        with client_socket:
+            print(f"Connected by {addr}")
+            keep_alive = True
+            while keep_alive:
                 request = client_socket.recv(1024).decode('utf-8')
-                response = handle_request(request)
+                if not request:
+                    break
+                response, keep_alive = handle_request(request)
                 client_socket.sendall(response.encode('utf-8'))
-                client_socket.close()
+
--- request_handler.py
+++ request_handler.py
@@ -1,4 +1,56 @@
+import base64
+
+AUTHORIZED_USERS = {"user1": "password1", "user2": "password2"}
+
+
 def handle_request(request):
-    # 这里是简化的请求处理逻辑
-    # 实际项目中需要根据请求的不同类型进行处理
-    return "HTTP/1.1 200 OK\nContent-Type: text/plain\n\nHello, World!"
+    headers = parse_headers(request)
+    keep_alive = False
+    if "Connection" in headers.keys():
+        keep_alive = headers.get("Connection").lower() != "close"
+
+    # Check for basic authorization header
+    if not is_authorized(headers):
+        return generate_unauthorized_response(), keep_alive
+
+    # Add your request handling logic here
+    response_headers = [
+        "HTTP/1.1 200 OK",
+        "Content-Type: text/plain",
+        "Content-Length: 13"
+    ]
+    if keep_alive:
+        response_headers.append("Connection: keep-alive")
+    response_body = "Hello, Authorized User!"
+    return "\\r\\n".join(response_headers) + "\\r\\n\\r\\n" + response_body, keep_alive
+
+
+def parse_headers(request):
+    headers = {}
+    lines = request.split("\\r\\n")
+    for line in lines[1:]:
+        if ": " in line:
+            key, value = line.split(": ", 1)
+            headers[key] = value
+    return headers
+
+
+def is_authorized(headers):
+    auth_header = headers.get("Authorization")
+    if not auth_header or not auth_header.startswith("Basic "):
+        return False
+    encoded_credentials = auth_header.split(" ")[1]
+    decoded_credentials = base64.b64decode(encoded_credentials).decode("utf-8")
+    username, password = decoded_credentials.split(":")
+    return username in AUTHORIZED_USERS and AUTHORIZED_USERS[username] == password
+
+
+def generate_unauthorized_response():
+    response_headers = [
+        "HTTP/1.1 401 Unauthorized",
+        "Content-Type: text/plain",
+        "WWW-Authenticate: Basic realm=\"Authorization Required\"",
+        "Content-Length: 23"
+    ]
+    response_body = "Unauthorized Access!"
+    return "\\r\\n".join(response_headers) + "\\r\\n\\r\\n" + response_body
