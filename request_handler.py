2d76b07481d9b801a20ae31845b391a1d4985f96
Daniel_on_WINDOWS <colinss.daniell530@gamil.com>
2023-12-21 18:51:53 +0800

    Cookie 完成。 调整解析格式。 TODO:优化解析结构

--- request_handler.py
+++ request_handler.py
@@ -1,10 +1,14 @@
 import base64
 import binascii
+import hashlib
 import mimetypes
 import os
 import urllib.parse
+import time
 
 AUTHORIZED_USERS = {"12110518": "asdasdasd", "user2": "password2", "client1": "123"}
+SESSIONS = {}
+SESSION_TIMEOUT = 600
 
 
 def handle_request(request):
@@ -29,74 +33,139 @@ def handle_request(request):
 
     method, path, _ = request_line.split(' ')
 
-    is_authed, currentUser = is_authorized(headers)
+    is_authed, currentUser, session_id = handle_user_auth(headers)
 
-    if method == "HEAD" and path == '/':
-        if not is_authed:
-            return generate_head_401_response(keep_alive), keep_alive
-        return generate_head_200_response(keep_alive), keep_alive
+    response_header = str()
+    response_body = bytes()
+
+
 
     # 检查授权
     if not is_authed:
-        return generate_401_response(keep_alive), keep_alive
+        response_header, response_body = generate_401_response(keep_alive)
 
-    if method == 'GET' and not path.startswith('/upload?'):
+    elif method == "HEAD" and path == '/':
+        response_header, response_body = generate_head_200_response(keep_alive)
+    elif method == "POST" and path == '/':
+        response_header, response_body = generate_200_response(keep_alive, "Post This")
+    elif method == 'GET' and not path.startswith('/upload?'):
 
         url_components = urllib.parse.urlparse(path)
         file_path = os.path.join('./data', url_components.path[1:])
         query_params = urllib.parse.parse_qs(url_components.query)
 
         if os.path.isdir(file_path):
-            return handle_directory_request(file_path, query_params, keep_alive, currentUser), keep_alive
+            response_header, response_body =  handle_directory_request(file_path, query_params, keep_alive, currentUser)
         elif os.path.isfile(file_path):
-            return generate_file_download_response(file_path, keep_alive), keep_alive
+            response_header, response_body =  generate_file_download_response(file_path, keep_alive)
         else:
-            return generate_404_response(keep_alive), keep_alive
+            response_header, response_body =  generate_404_response(keep_alive)
 
     elif method == 'POST' and not path.startswith('/upload?') and not path.startswith('/delete?'):
-        return generate_400_response(keep_alive), keep_alive
+        response_header, response_body =  generate_400_response(keep_alive)
 
     elif method != 'POST' and path.startswith('/upload?'):
-        return generate_405_response(keep_alive), keep_alive
+        response_header, response_body =  generate_405_response(keep_alive)
 
     elif method == 'POST' and path.startswith('/upload?'):
         parsed_url = urllib.parse.urlparse(path)
         query_params = urllib.parse.parse_qs(parsed_url.query)
         if 'path' not in query_params:
-            return generate_400_response(keep_alive), keep_alive
+            response_header, response_body = generate_400_response(keep_alive)
+        else:
+            path = query_params['path'][0].lstrip('/')
+            directory = path.strip('/').split('/')[0]  # 假设目录是路径的第一部分
+            if directory != currentUser:
+                path = query_params['path'][0].lstrip('/')
+                directory = path.strip('/').split('/')[0]  # 假设目录是路径的第一部分
+                response_header, response_body = generate_403_response(keep_alive)
+            elif directory not in AUTHORIZED_USERS.keys():
+                response_header, response_body = generate_404_response(keep_alive)
 
-        path = query_params['path'][0].lstrip('/')
-        directory = path.strip('/').split('/')[0]  # 假设目录是路径的第一部分
-
-        if directory != currentUser:
-            return generate_403_response(keep_alive), keep_alive
-
-        if directory not in AUTHORIZED_USERS.keys():
-            return generate_404_response(keep_alive), keep_alive
-
-        return handle_post_request(request, headers, './data/' + path, keep_alive, currentUser)
+            else:
+                response_header, response_body = handle_post_request(request, headers, './data/' + path, keep_alive, currentUser)
     elif method == 'POST' and path.startswith('/delete?'):
 
         parsed_url = urllib.parse.urlparse(path)
         query_params = urllib.parse.parse_qs(parsed_url.query)
 
         if 'path' not in query_params:
-            return generate_400_response(keep_alive), keep_alive
+            response_header, response_body =  generate_400_response(keep_alive)
+        else:
+            path = query_params['path'][0].lstrip('/')
+            directory = path.strip('/').split('/')[0]  # 假设目录是路径的第一部分
+            real_path = './data/' + path
 
-        path = query_params['path'][0].lstrip('/')
-        directory = path.strip('/').split('/')[0]  # 假设目录是路径的第一部分
+            if directory != currentUser:
+                response_header, response_body = generate_403_response(keep_alive)
+            elif not os.path.exists(real_path):
+                response_header, response_body = generate_404_response(keep_alive)
+            else:
+                response_header, response_body = handle_delete_request(real_path, keep_alive)
+    else:
+        response_header, response_body = generate_405_response(keep_alive), keep_alive
 
-        if directory != currentUser:
-            return generate_403_response(keep_alive), keep_alive
+    if not is_authed:
+        if method == "HEAD":
+            return (response_header + '\r\n\r\n').encode('utf-8'), keep_alive
+        else:
+            return (response_header + '\r\n\r\n').encode('utf-8') + response_body, keep_alive
+    elif currentUser and session_id:
+        if method == "HEAD":
+            return (response_header + f"\r\nSet-Cookie: session-id={session_id}; Path=/; HttpOnly;" + '\r\n\r\n').encode('utf-8'), keep_alive
+        else:
+            return (response_header + f"\r\nSet-Cookie: session-id={session_id}; Path=/; HttpOnly;" + '\r\n\r\n').encode('utf-8') + response_body, keep_alive
+    else:
+        if method == "HEAD":
+            return (response_header + '\r\n\r\n').encode('utf-8'), keep_alive
+        else:
+            return (response_header + '\r\n\r\n').encode('utf-8') + response_body, keep_alive
 
-        real_path = './data/' + path
 
-        print(real_path)
+def is_session_valid(session_id):
+    if session_id in SESSIONS.keys():
+        if time.time() < SESSIONS[session_id]['expiry']:
+            return True
+        else:
+            # Session expired
+            del SESSIONS[session_id]
+    return False
 
-        if not os.path.exists(real_path):
-            return generate_404_response(keep_alive), keep_alive
+def handle_user_auth(headers):
+    cookies = headers.get('Cookie', '')
+    session_id = None
+    if cookies and 'session-id=' in cookies:
+        session_id = cookies.split('session-id=')[1].split(';')[0]
+        if session_id and is_session_valid(session_id):
+            # User is authenticated via session
+            is_authed = True
+            current_user = get_user_from_session(session_id)
+            return is_authed, current_user, None
 
-        return handle_delete_request(real_path, keep_alive)
+
+    is_authed, currentUser = is_authorized(headers)
+    if is_authed:
+        session_id = generate_session_id(currentUser)
+        return is_authed, currentUser, session_id
+    return is_authed, None, None
+
+
+def generate_session_id(username):
+    current_time = str(time.time()).encode('utf-8')
+    user_bytes = username.encode('utf-8')
+    session_id = hashlib.sha256(user_bytes + current_time).hexdigest()
+    SESSIONS[session_id] = {'user': username, 'expiry': time.time() + SESSION_TIMEOUT}
+    return session_id
+
+def get_user_from_session(session_id):
+    if session_id in SESSIONS:
+        session_info = SESSIONS[session_id]
+        if time.time() < session_info['expiry']:
+            return session_info['user']
+        else:
+            # Session expired
+            del SESSIONS[session_id]
+    return None
 
 
 def handle_delete_request(real_path, keep_alive):
@@ -110,17 +179,17 @@ def handle_delete_request(real_path, keep_alive):
 
 def handle_post_request(request, headers, file_path, keep_alive, currentUser):
     if not 'Content-Type' in headers.keys():
-        return generate_400_response(keep_alive), keep_alive
+        return generate_400_response(keep_alive)
 
     content_type = headers['Content-Type']
     if 'multipart/form-data' not in content_type:
-        return generate_400_response(keep_alive), keep_alive
+        return generate_400_response(keep_alive)
 
     # Extract boundary from content type header
     boundary = content_type.split(';')[1].strip().split('=')[1]
 
     # Extract and save the file
-    return extract_and_save_file(request, boundary, file_path, keep_alive), keep_alive
+    return extract_and_save_file(request, boundary, file_path, keep_alive)
 
 
 def extract_and_save_file(request, boundary, file_path, keep_alive):
@@ -169,7 +238,7 @@ def generate_directory_list_response(file_path, keep_alive):
     ]
     if keep_alive:
         response_headers.append("Connection: keep-alive")
-    return ("\r\n".join(response_headers) + "\r\n\r\n" + str(items)).encode('utf-8')
+    return "\r\n".join(response_headers), str(items).encode('utf-8')
 
 
 def generate_html_directory_response(path, base_url, keep_alive, currentUser):
@@ -201,7 +270,7 @@ def generate_html_directory_response(path, base_url, keep_alive, currentUser):
     ]
     if keep_alive:
         response_headers.append("Connection: keep-alive")
-    return ("\r\n".join(response_headers) + "\r\n\r\n" + html_content).encode('utf-8')
+    return "\r\n".join(response_headers), html_content.encode('utf-8')
 
 
 def parse_headers(request):
@@ -237,7 +306,25 @@ def generate_file_list_response(keep_alive):
     ]
     if keep_alive:
         response_headers.append("Connection: keep-alive")
-    return ("\r\n".join(response_headers) + "\r\n\r\n" + response_body).encode('utf-8')
+    return "\r\n".join(response_headers), response_body.encode('utf-8')
+
+
+def generate_head_200_cookie_response(keep_alive, session_id=None):
+    headers = "HTTP/1.1 200 OK\r\n"
+    if session_id:
+        headers += f"Set-Cookie: session-id={session_id}; HttpOnly\r\n"
+    # ... rest of the header and response generation ...
+    return "\r\n".join(headers)
+
+
+def generate_200_cookie_response(keep_alive, session_id=None):
+    headers = "HTTP/1.1 200 OK\r\n"
+    content = 'Cookie is set.'
+    if session_id:
+        headers += f"Set-Cookie: session-id={session_id}; HttpOnly\r\n"
+        headers += f"Content-Length: {len(content)}"
+    # ... rest of the header and response generation ...
+    return "\r\n".join(headers), content.encode('utf-8')
 
 
 def generate_file_download_response(path, keep_alive):
@@ -257,7 +344,7 @@ def generate_file_download_response(path, keep_alive):
     ]
     if keep_alive:
         response_headers.append("Connection: keep-alive")
-    return ("\r\n".join(response_headers) + "\r\n\r\n").encode('utf-8') +response_body
+    return "\r\n".join(response_headers), response_body
 
 
 def generate_401_response(keep_alive):
@@ -271,7 +358,7 @@ def generate_401_response(keep_alive):
     if keep_alive:
         response_headers.append("Connection: keep-alive")
     response_body = "Unauthorized Access!"
-    return ("\r\n".join(response_headers) + "\r\n\r\n" + response_body).encode('utf-8')
+    return "\r\n".join(response_headers), response_body.encode('utf-8')
 
 
 def generate_400_response(keep_alive):
@@ -283,7 +370,7 @@ def generate_400_response(keep_alive):
     if keep_alive:
         response_headers.append("Connection: keep-alive")
     response_body = "File not found"
-    return ("\r\n".join(response_headers) + "\r\n\r\n" + response_body).encode('utf-8')
+    return "\r\n".join(response_headers), response_body.encode('utf-8')
 
 def generate_403_response(keep_alive):
     response_headers = [
@@ -294,7 +381,7 @@ def generate_403_response(keep_alive):
     if keep_alive:
         response_headers.append("Connection: keep-alive")
     response_body = "No Authorized!"
-    return ("\r\n".join(response_headers) + "\r\n\r\n" + response_body).encode('utf-8')
+    return "\r\n".join(response_headers), response_body.encode('utf-8')
 
 
 def generate_405_response(keep_alive):
@@ -306,7 +393,7 @@ def generate_405_response(keep_alive):
     if keep_alive:
         response_headers.append("Connection: keep-alive")
     response_body = "Method not allowed"
-    return ("\r\n".join(response_headers) + "\r\n\r\n" + response_body).encode('utf-8')
+    return "\r\n".join(response_headers), response_body.encode('utf-8')
 
 
 def generate_404_response(keep_alive):
@@ -318,7 +405,7 @@ def generate_404_response(keep_alive):
     if keep_alive:
         response_headers.append("Connection: keep-alive")
     response_body = "File not found"
-    return ("\r\n".join(response_headers) + "\r\n\r\n" + response_body).encode('utf-8')
+    return "\r\n".join(response_headers), response_body.encode('utf-8')
 
 
 def generate_200_response(keep_alive, response_body):
@@ -329,7 +416,7 @@ def generate_200_response(keep_alive, response_body):
     ]
     if keep_alive:
         response_headers.append("Connection: keep-alive")
-    return ("\r\n".join(response_headers) + "\r\n\r\n" + response_body).encode('utf-8')
+    return "\r\n".join(response_headers), response_body.encode('utf-8')
 
 
 def generate_500_response(keep_alive):
@@ -341,7 +428,7 @@ def generate_500_response(keep_alive):
     if keep_alive:
         response_headers.append("Connection: keep-alive")
     response_body = "Delete file failed."
-    return ("\r\n".join(response_headers) + "\r\n\r\n" + response_body).encode('utf-8')
+    return "\r\n".join(response_headers), response_body.encode('utf-8')
 
 
 def generate_head_401_response(keep_alive):
@@ -352,7 +439,7 @@ def generate_head_401_response(keep_alive):
     ]
     if keep_alive:
         response_headers.append("Connection: keep-alive")
-    return ("\r\n".join(response_headers) + "\r\n\r\n").encode('utf-8')
+    return "\r\n".join(response_headers), None
 
 
 def generate_head_200_response(keep_alive):
@@ -363,4 +450,4 @@ def generate_head_200_response(keep_alive):
     ]
     if keep_alive:
         response_headers.append("Connection: keep-alive")
-    return ("\r\n".join(response_headers) + "\r\n\r\n").encode('utf-8')
+    return "\r\n".join(response_headers), None
